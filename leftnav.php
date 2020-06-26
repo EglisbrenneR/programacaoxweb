8a7caf77923bd6f39adb0c50a8178efe14f5034f
EglisbrenneR <62037854+EglisbrenneR@users.noreply.github.com>
2020-06-26 16:44:46 -0300

    Add files via upload

---
+++ leftnav.php
@@ -0,0 +1,111 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">PROJETO X</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="https://instagram.ffor1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/87231112_202764620796947_8480856948260995072_n.jpg?_nc_ht=instagram.ffor1-2.fna.fbcdn.net&_nc_ohc=_6GpW5YJHQ8AX8MmCpv&oh=87d2fc9186b2e493ba8dd46dff1caa76&oe=5F1F234D" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">EGLISBRENNER BORGES</a>
+        </div>
+      </div>
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+               <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                ATIVIDADE PHP
+                <span class="right badge badge-danger">NOVA</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+               QUESTÕES
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./questao1.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 1</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao2.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 2</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao3.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 3</p>
+                </a>
+              </li>
+             <li class="nav-item">
+                <a href="./questao4.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 4</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao5.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 5</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao6.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 6</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao7.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 7</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao8.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 8</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao9.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 9</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./questao10.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>QUESTÃO 10</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
